9721a85fa34a449d33f570662216aa3bfb100d92
Jonatan Steuernagel <jonatan@jsteuernagel.de>
2021-08-27 10:51:06 +0200

    Final changes for demo

--- multi.py
+++ multi.py
@@ -5,14 +5,14 @@ from homie.device_integer import Device_Integer
 import sys
 
 mqtt_settings = {
-    "MQTT_BROKER": "ezmeral52gateway1.demo.local",
-    "MQTT_PORT": 10003,
+    "MQTT_BROKER": "stable201.container.demo.local",
+    "MQTT_PORT": 10106,
     "MQTT_USERNAME": "ezmeral",
     "MQTT_PASSWORD": "",
     "MQTT_KEEPALIVE": 60,
     "MQTT_CLIENT_ID": None,
     "MQTT_SHARE_CLIENT": True,
-    "MQTT_USE_TLS": True,
+    "MQTT_USE_TLS": False,
 }
 
 class Weight_Sensor(Device_Integer):
@@ -33,9 +33,9 @@ for x in range(1, 10):
     )
     sensors.append(sensor)
 
-EMULATE_HX711=True
+EMULATE_HX711=False
 
-referenceUnit = 1
+referenceUnit = 100
 
 if not EMULATE_HX711:
     import RPi.GPIO as GPIO
@@ -53,15 +53,32 @@ def cleanAndExit():
     sys.exit()
 
 hx = []
+print("Adding 1")
+hx.append(HX711(4, 25))
+print("Adding 2")
+hx.append(HX711(24, 23))
+print("Adding 3")
+hx.append(HX711(22, 27))
+print("Adding 4")
+hx.append(HX711(10, 9))
+print("Adding 5")
 hx.append(HX711(5, 6))
-hx.append(HX711(7, 8))
+print("Adding 6")
+hx.append(HX711(8, 7))
+print("Adding 7")
+hx.append(HX711(18, 17))
+print("Adding 8")
+hx.append(HX711(21, 20))
+print("Adding 9")
+hx.append(HX711(19, 13))
 
 for dev in hx:
     dev.set_reading_format("MSB", "MSB")
     dev.set_reference_unit(referenceUnit)
 
     dev.reset()
-    dev.tare()
+    dev.tare(20)
+    print("Tare done for device")
 
 print("Tares done! Add weight now...")
 
@@ -72,11 +89,11 @@ while True:
             time.sleep(0.1)
 
             val = hx[i].get_weight(5)
-            print("Weight " + str(i) + ": " + str(val))
+            #print("Weight " + str(i) + ": " + str(val))
             sensors[i].update_value(val)
 
             hx[i].power_down()
-            time.sleep(5)
+            time.sleep(1)
 
     except (KeyboardInterrupt, SystemExit):
         cleanAndExit()
